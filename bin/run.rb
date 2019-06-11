c18e1d0040a8dbf925c25d825da260abceb739d1
github username <github email address>
2019-06-11 14:27:53 -0400

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,11 @@
+require_relative '../config/environment'
 Hashie
 Sinatra
 Octokit
 Pry
 AwesomePrint
+require('rspec')
+require('sinatra')
+require('hashie')
+require('pry')
+require('awesome_print')
